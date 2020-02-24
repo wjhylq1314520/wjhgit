5e73fd73c758c3c49199fdba9ab0686e334cbfdf
wjh <1648472004@qq.com>
2020-02-24 16:02:58 +0800

    my commit

--- src/main/java/nj/pj/service/UserService.java
+++ src/main/java/nj/pj/service/UserService.java
@@ -2,9 +2,9 @@ package nj.pj.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 
-import nj.pj.mapper.UserMapper;
+
+
 import nj.pj.pojo.User;
 
 public interface UserService {
